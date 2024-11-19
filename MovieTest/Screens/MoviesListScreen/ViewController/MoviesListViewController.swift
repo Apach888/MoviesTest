c4d4ca663153438757faab8ddb5bd133e99149c1
Dima Protsak <>
2024-11-19 13:18:46 +0100

    Added update

--- MovieTest/Screens/MoviesListScreen/ViewController/MoviesListViewController.swift
+++ MovieTest/Screens/MoviesListScreen/ViewController/MoviesListViewController.swift
@@ -132,7 +132,11 @@ final class MoviesListViewController: UIViewController {
         var snapshot = Snapshot()
         snapshot.appendSections([0])
         snapshot.appendItems(movies)
-        dataSource.apply(snapshot, animatingDifferences: true)
+        dataSource.apply(snapshot, animatingDifferences: true) { [weak self] in
+            guard let self else { return }
+            let firstIndexPath = IndexPath(item: 0, section: 0)
+            collectionView.scrollToItem(at: firstIndexPath, at: .top, animated: true)
+        }
     }
 }
 
